9b1954741cc7b7f364e09ca3e5514a42d3b47b2a
Thomas Moras <mt407677@etu.unice.fr>
2015-11-13 11:12:07 +0100

    Nouveau import

--- src/main/java/eu/ace_design/island/mvp/Explorer.java
+++ src/main/java/eu/ace_design/island/mvp/Explorer.java
@@ -1,5 +1,7 @@
 package eu.ace_design.island.mvp;
 
+import eu.ace_design.island.bot.IExplorerRaid;
+
 public class Explorer implements IExplorerRaid {
     public void initialize(String string) {
         
