2f234ef43b5e7ad453b460fe709a107a6045c90c
Thomas Jalabert <pol-jalabe@atlas.i3s.unice.fr>
2015-11-18 18:32:55 +0100

    Gestion des réponses de la map en cours #QAE-28 #QAE-26

---
+++ src/test/java/fr/unice/polytech/qgl/qae/reply/AbstractReplyTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.unice.polytech.qgl.qae.reply;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class AbstractReplyTest {
+    AbstractReply ar;
+    
+    public AbstractReplyTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        ar = new FlyReply("{\"cost\":2,\"extras\":{},\"status\":\"OK\"}");
+    }
+
+    /**
+     * Test of getCost method, of class AbstractReply.
+     */
+    @Test
+    public void testGetCost() {
+        assertEquals(ar.getCost(), 2);
+    }
+
+    /**
+     * Test of getStatus method, of class AbstractReply.
+     */
+    @Test
+    public void testGetStatus() {
+        assertEquals(ar.getStatus(), true);     
+    }
+    
+    /**
+     * Test of addextra method, of class AbstractReply.
+     */
+    @Test
+    public void testAddextra() {
+        
+    }
+    
+}
