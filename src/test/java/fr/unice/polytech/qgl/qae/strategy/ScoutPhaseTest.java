0317fb7a8447d05be04a03b55f8bb304b6cd27fe
Thomas Moras <mt407677@etu.unice.fr>
2016-01-03 18:35:14 +0100

    Correctif ScoutPhaseTest ?

---
+++ src/test/java/fr/unice/polytech/qgl/qae/strategy/ScoutPhaseTest.java
@@ -0,0 +1,32 @@
+package fr.unice.polytech.qgl.qae.strategy;
+
+import fr.unice.polytech.qgl.qae.Objectif;
+import fr.unice.polytech.qgl.qae.actions.flyActions.withparams.Direction;
+import fr.unice.polytech.qgl.qae.map.Map;
+import fr.unice.polytech.qgl.qae.map.geometry.Coordinates;
+import org.junit.Before;
+
+/**
+ * Created by user on 03/01/16.
+ */
+public class ScoutPhaseTest {
+
+    AbstractStrategy ex;
+    AbstractPhase ph0,ph1;
+    Objectif ob;
+
+    public ScoutPhaseTest() {
+    }
+
+    @Before
+    public void setUp() {
+        Map m = new Map();
+        ex = new FlyingStrategy(Direction.E, ob);
+        ph0 = new InitTerrestre(ex,new Coordinates(0, 0),Direction.E, m);
+        ph1 = new MovePhase(ex,new Coordinates(0, 0), Direction.E, m);
+
+    }
+
+}
+
+
