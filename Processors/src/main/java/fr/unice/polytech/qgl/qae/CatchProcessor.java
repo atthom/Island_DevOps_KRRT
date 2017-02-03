29f1ef7eea2ab10aa3e8a7eee97cc6243f70f013
Thomas Jalabert <thom.jalabert@gmail.com>
2017-02-03 16:11:11 +0100

    push new module

--- Processors/src/main/java/fr/unice/polytech/qgl/qae/CatchProcessor.java
+++ Processors/src/main/java/fr/unice/polytech/qgl/qae/CatchProcessor.java
@@ -1,8 +1,5 @@
 package fr.unice.polytech.qgl.qae;
 
-/**
- * Created by Thom on 03/02/2017.
- */
 import org.apache.log4j.Level;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtCatch;
@@ -10,9 +7,9 @@ import spoon.reflect.code.CtCatch;
 /**
  * Reports warnings when empty catch blocks are found.
  */
-class CatchProcessor extends AbstractProcessor<CtCatch> {
+public class CatchProcessor extends AbstractProcessor<CtCatch> {
     public void process(CtCatch element) {
-        if (element.getBody().getStatements().size() > 0) {
+        if (element.getBody().getStatements().size() == 0) {
             getFactory().getEnvironment().report(this, Level.WARN, element, "empty catch clause");
         }
     }
