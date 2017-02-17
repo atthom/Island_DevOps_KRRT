dd7f7c092f19275b62b84e83f380e45ab39a65f4
Thomas Jalabert <thom.jalabert@gmail.com>
2017-02-17 15:48:56 +0100

    Implement Relation Inverter for most Binaryrelation on the whole project.

---
+++ Processors/src/main/java/fr/unice/polytech/devops/g1/BinaryRelationInverter.java
@@ -0,0 +1,19 @@
+package fr.unice.polytech.devops.g1;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtExpression;
+
+
+/**
+ * Created by Thom on 17/02/2017.
+ */
+public class BinaryRelationInverter extends AbstractProcessor<CtBinaryOperator> {
+    public void process(CtBinaryOperator element) {
+        CtExpression left = element.getLeftHandOperand();
+        element.setLeftHandOperand(element.getRightHandOperand());
+        element.setRightHandOperand(left);
+
+
+    }
+}
